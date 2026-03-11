375623908ec1e3d3221d470e2bf7b02cdccc8ffd
Romaisa <romaisablbgn@gmail.com>
2026-03-11 10:26:22 +0800

    Update copyright comment format in copyright.js

--- _includes/copyright.js
+++ _includes/copyright.js
@@ -1,6 +1,1 @@
-/*!
- * Minimal Mistakes Jekyll Theme 4.27.3 by Michael Rose
- * Copyright 2013-2025 Michael Rose - mademistakes.com | @mmistakes
- * Free for personal and commercial use under the MIT license
- * https://github.com/mmistakes/minimal-mistakes/blob/master/LICENSE
- */
+/* Created with Minimal Mistakes Jekyll Theme 4.27.3 by Michael Rose */
